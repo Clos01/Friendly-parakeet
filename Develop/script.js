31bb713726be604131cd06bf9c0e18c1b8c42de2
clos01 <cjrivas00@gmail.com>
2022-01-19 13:24:08 -0500

    done with var upload now

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,39 @@
 // Assignment code here
+function generatePassword () {
+  console.log ("is it working?");
 
+// 1. Need to do promt//
+var generatePassword = prompt("how many Characters would you like your password to conain?");
+
+var generatePassword = prompt ("Click ok to Confirm including special characters"); { 
+console.log("working");
+};
+
+var generatePassword = prompt ("Click ok to Confirm including numberic characters"); { 
+  console.log("working");
+  };
+
+  var generatePassword = prompt ("Click ok to Confirm including lowercase characters"); { 
+    console.log("working");
+    };
+    
+    var generatePassword = prompt ("Click ok to Confirm including uppercase characters"); { 
+      console.log("working");
+      };
+
+// A. Need to make password length between 8 and 28//
+// B. lowercase uppercase numbers special characters//
+
+// 2. need to validate input at least one character is selceted//
+
+// 3. Generate password//
+
+
+
+// 4. Display password to the page  
+
+return "look at this" ;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +49,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+    
